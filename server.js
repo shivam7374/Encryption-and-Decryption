a77ff00231862ae0b39a7baf38eec4a0195db8cd
shivam7374 <shivamsingla491@gmail.com>
2020-05-05 02:38:41 +0530

    Encrypt And Decrypt

--- server.js
+++ server.js
@@ -7,7 +7,41 @@ app.use('/', express.static(__dirname + '/public'))
 function decryptQueryParams(req, res ,next) {
 
     // TODO: decrypt all query params as per our logic
+    
+    console.log(req.query)
 
+    for (let q in req.query) 
+    {
+            
+        let data = req.query[q] 
+        // for (let i in data) 
+    //  {
+    //     console.log(data[i]+'\n')
+    //     // data = new Buffer(data, 'base64').toString('ascii')
+    //     // let lower=/([a-z]+)/g
+    //     // let upper=/([A-Z]+)/g
+    //     if(isNaN(data[i] * 1))
+    //     {
+    //     if(data[i]==data[i].toLowerCase())
+    //     {
+    //         data[i]=data[i].toUpperCase()
+    //     }
+    //     else if(data[i]==data[i].toUpperCase())
+    //     {
+    //         data[i]=data[i].toLowerCase()
+    //     }
+    //     }
+    //  }
+    swapcase = function swapcase(str) {
+        return str.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
+            return chr ? match.toUpperCase() : match.toLowerCase();
+        });
+    }
+        req.query[q] = swapcase(data)
+    }
+    console.log(req.query)
+    
+    
     next()
 }
 
@@ -20,7 +54,7 @@ function decodeQueryBase64(req, res, next) {
     next()
 }
 
-app.get('/eval',  decodeQueryBase64, (req, res) => {
+app.get('/eval', decryptQueryParams,decodeQueryBase64, (req, res) => {
     console.log(req.query)
     let s=req.query.data
     // TODO: eval the code actually
