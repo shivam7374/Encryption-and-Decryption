0ec06a8f48b45444b1434d8938dc76a553e52680
shivam7374 <shivamsingla491@gmail.com>
2020-05-05 13:57:23 +0530

    Encrypt and Decrypt

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+// app.use('/', express.static(__dirname+'/public'))
+
 app.use('/', express.static(__dirname))
 
 function decryptQueryParams(req, res ,next) {
